f7a5e0015074609b837f250296e4ddd383584ffa
Kadir Tuncbilek <ktuncbil@k2m15s07.42kocaeli.com.tr>
2022-02-27 15:18:51 +0300

    Libft

--- ft_memset.c
+++ ft_memset.c
@@ -6,14 +6,14 @@
 /*   By: ktuncbil <ktuncbil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 16:31:03 by ktuncbil          #+#    #+#             */
-/*   Updated: 2022/02/26 18:32:31 by ktuncbil         ###   ########.fr       */
+/*   Updated: 2022/02/27 15:18:39 by ktuncbil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 /*Copies the character c to the first n characters of the string pointed to, by 
-the argument str.*/
+the argument dest.*/
 
 void	*ft_memset(void *dest, int c, size_t len)
 {
